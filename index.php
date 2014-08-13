84f4ebd52945cd53d06db9a2a6792a3857d251f3
Callum Macdonald <callum@redevolution.com>
2014-08-13 16:54:51 +0100

    initial commit
    Signed-off-by: Callum Macdonald <callum@redevolution.com>

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<title>Grampian Emerald CSC | Aberdeen</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta name="description" content="Grampian Emerald Celtic Supporters Club, Aberdeen, Scotland">
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<script src="//code.jquery.com/jquery-1.10.2.min.js"></script>    
+<script src="assets/js/bootstrap.min.js"></script>    
+<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">     
+<link rel="stylesheet" type="text/css" href="assets/css/style.css">       
+</head>
+
+<body>
+
+<?php include("includes/constants/header.html");?>
+
+<?php include("includes/content/home.html");?>
+
+<?php include("includes/constants/footer.html");?>
+
+
+
+</body>
+</html>
