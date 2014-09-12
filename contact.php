fba32b2b2e53c899e51a8dfba390bc492e18739f
Callum Macdonald <callum@redevolution.com>
2014-09-12 13:12:03 +0100

    pre dev-technology deployment work
    Signed-off-by: Callum Macdonald <callum@redevolution.com>

---
+++ contact.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<title>Grampian Emerald CSC | Aberdeen</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta name="description" content="Grampian Emerald Celtic Supporters Club, Aberdeen, Scotland">
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<script src="//code.jquery.com/jquery-1.10.2.min.js"></script>    
+<script src="assets/js/bootstrap.min.js"></script>    
+<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">     
+
+<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->    
+
+<link rel="stylesheet" type="text/css" href="assets/css/style.css">    
+<script src="email/validation.js" type="text/javascript"></script>    
+    
+</head>
+
+<body>
+
+<?php include("includes/constants/header.html");?>
+
+<?php include("includes/content/contact.html");?>
+
+<?php include("includes/constants/footer.html");?>
+
+
+
+</body>
+</html>
